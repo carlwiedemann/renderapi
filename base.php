2ef39286132b07a29a8156254804d62632dbaf9e
Carl Wiedemann <carl.wiedemann@gmail.com>
2014-05-30 10:45:37 -0700

    Use toString for now

--- base.php
+++ base.php
@@ -28,7 +28,7 @@ function delegate_response($build, Request $request, Application $app) {
   }
   else {
     // Return rendered HTML.
-    return $build->render();
+    return (string) $build;
   }
 }
 
