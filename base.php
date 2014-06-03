044676f2bfbfcb1c72fd6be47e9d3474722b803f
Carl Wiedemann <carl.wiedemann@gmail.com>
2014-06-03 15:00:09 -0500

    Rename the accessor variables

--- base.php
+++ base.php
@@ -13,12 +13,12 @@ use Symfony\Component\HttpFoundation\Request;
  * Provides a Silex response per rendered HTML, or an JSON response.
  */
 function delegate_response($build, Request $request, Application $app) {
-  // If we have ?path set, we'll return as JSON.
-  if ($request->query->get('path')) {
-    // If we have ?themed set, we'll delegate to Renderables.
-    $accessor = Accessor::create($build, $request->query->get('themed'));
-    // Parse path to send to accessor.
-    $params = array_filter(explode('.', $request->query->get('path')), function($a) { return $a !== ''; });
+  // If we have ?rendervar set, we'll return as JSON.
+  if ($request->query->get('rendervar')) {
+    // If we have ?prepare set, we'll delegate to Renderables.
+    $accessor = Accessor::create($build, $request->query->get('prepare'));
+    // Parse drill to send to accessor.
+    $params = array_filter(explode('.', $request->query->get('rendervar')), function($a) { return $a !== ''; });
     // Churn through accessor.
     foreach ($params as $param) {
       $accessor = $accessor->get($param);
